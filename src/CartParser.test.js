24436d1975b5254064439416d12a8c0ff229d167
Valentyn13 <valentyn13git@gmail.com>
2024-02-03 20:17:31 +0200

    test and mock data added

--- src/CartParser.test.js
+++ src/CartParser.test.js
@@ -1,15 +1,126 @@
-import CartParser from './CartParser';
+import CartParser from "./CartParser";
+import {
+  parsedLines,
+  OUTPUT,
+  EXPECTED_CSV_FILE_DATA,
+  DATA_WITH_NEGATIVE_CELL,
+  DATA_WITH_BAD_CELLS_NUMBER,
+  DATA_WITH_EMPTY_CELL,
+  DATA_WITH_INCORRECT_HEADER,
+  BUNCH_OF_BROKEN_DATA,
+} from "./mocs/mockData";
+import { v4 } from "uuid";
 
 let parser;
+const CSV_FILE_PATH = "./samples/cart.csv";
+
+jest.mock("uuid");
 
 beforeEach(() => {
-	parser = new CartParser();
+  parser = new CartParser();
+
+  v4.mockReturnValue("12345");
 });
 
-describe('CartParser - unit tests', () => {
-	// Add your unit tests here.
+describe("CartParser - unit tests", () => {
+  // 1
+  test("parser.readFile method should return correct data", () => {
+    const fileData = parser.readFile(CSV_FILE_PATH);
+    expect(fileData).toBe(EXPECTED_CSV_FILE_DATA);
+  });
+
+  // 2
+  test("parser.readFile method should throw error", () => {
+    expect(() => parser.readFile("bad path", "utf-8").toThrowError());
+  });
+
+  // 3
+  test("parser.validate method shouldn't throw error", () => {
+    const fileData = parser.readFile(CSV_FILE_PATH);
+    const validationErrors = parser.validate(fileData);
+    expect(validationErrors.length).toBe(0);
+  });
+
+  // 4
+  test('parser.validate method should throw "negative cell" error', () => {
+    const validationErrors = parser.validate(DATA_WITH_NEGATIVE_CELL);
+    expect(validationErrors.length).toBe(1);
+    expect(validationErrors[0].message).toStrictEqual(
+      `Expected cell to be a positive number but received "-4.4".`
+    );
+  });
+
+  // 5
+  test('parser.validate method should throw "incorrect cells number" error', () => {
+    const validationErrors = parser.validate(DATA_WITH_BAD_CELLS_NUMBER);
+    expect(validationErrors.length).toBe(1);
+    expect(validationErrors[0].message).toStrictEqual(
+      `Expected row to have 3 cells but received 2.`
+    );
+  });
+
+  // 6
+  // ERROR TEST(but I don't know how to trigger it)
+
+    // test('parser.validate method should throw "empty cell" error', () => {
+    //   const validationErrors = parser.validate(DATA_WITH_EMPTY_CELL);
+    //   expect(validationErrors.length).toBe(1);
+    //   expect(validationErrors[0].message).toStrictEqual(`Expected cell to be a nonempty string but received "".`);
+    // });
+
+  // 7
+  test('parser.validate method should throw "incorrect header" error', () => {
+    const validationErrors = parser.validate(DATA_WITH_INCORRECT_HEADER);
+    expect(validationErrors.length).toBe(1);
+    expect(validationErrors[0].message).toStrictEqual(
+      'Expected header to be named "Quantity" but received Quan_tity.'
+    );
+  });
+
+  // 8
+  test("parser.parseLine method should parse all lines correctly", () => {
+    const lines = EXPECTED_CSV_FILE_DATA.split(/\n/)
+      .filter((l) => l)
+      .filter((l, i) => i > 0);
+    for (let i = 0; i < lines.length; i++) {
+      const parsedLine = parser.parseLine(lines[i]);
+      expect(parsedLine).toStrictEqual(parsedLines[i]);
+    }
+  });
+
+  // 9
+  test("parser.calcTotal should work correctly", () => {
+    const total = parser.calcTotal(parsedLines);
+    expect(total).toBe(357.31999999999994);
+  });
+
+  // 10
+  test("parser.createError should return correct error object", () => {
+    const error = {
+      type: "header",
+      row: 2,
+      column: 3,
+      message: "error message",
+    };
+    expect(parser.createError("header", 2, 3, "error message")).toStrictEqual(
+      error
+    );
+  });
 });
 
-describe('CartParser - integration test', () => {
-	// Add your integration test here.
+describe("CartParser - integration test", () => {
+  test("parser.parse should work correctly", () => {
+    const result = parser.parse(CSV_FILE_PATH);
+    expect(result).toStrictEqual(OUTPUT);
+  });
+
+  // 11
+  
+  //   test("parser.parse should throw errors", () => {
+  // 	for (const data of BUNCH_OF_BROKEN_DATA) {
+  // 	  expect(() =>
+  // 		expect(parser.parse(data)).toThrowError(/^Validation failed!$/)
+  // 	  );
+  // 	}
+  //   });
 });
---
+++ src/mocs/mockData.js
@@ -0,0 +1,89 @@
+export const EXPECTED_CSV_FILE_DATA =
+  "Product name,Price,Quantity\r\nMollis consequat,9.00,3\r\nTvoluptatem,10.32,1\r\nScelerisque lacinia,18.90,1\r\nConsectetur adipiscing,28.72,10\r\nCondimentum aliquet,13.90,1\r\n";
+
+export const DATA_WITH_NEGATIVE_CELL =
+  "Product name,Price,Quantity\r\nMollis consequat,9.00,3\r\nTvoluptatem,10.32,1\r\nScelerisque lacinia,-4.4,1\r\nConsectetur adipiscing,28.72,10\r\nCondimentum aliquet,13.90,1\r\n";
+
+export const DATA_WITH_BAD_CELLS_NUMBER =
+  "Product name,Price,Quantity\r\nMollis consequat,5,\r\nTvoluptatem,10.32,1\r\nScelerisque lacinia,18.90,1\r\nConsectetur adipiscing,28.72,10\r\nCondimentum aliquet,13.90,1\r\n";
+
+export const DATA_WITH_EMPTY_CELL =
+`Product name,Price,Quantity\n,10.00,2`;
+
+export const DATA_WITH_INCORRECT_HEADER =
+  "Product name,Price,Quan_tity\r\n Kukuruzka,5,2\r\nTvoluptatem,10.32,1\r\nScelerisque lacinia,18.90,1\r\nConsectetur adipiscing,28.72,10\r\nCondimentum aliquet,13.90,1\r\n";
+
+export const BUNCH_OF_BROKEN_DATA = [
+  DATA_WITH_NEGATIVE_CELL,
+  DATA_WITH_BAD_CELLS_NUMBER,
+  DATA_WITH_INCORRECT_HEADER,
+];
+
+export const OUTPUT = {
+  items: [
+    {
+      name: "Mollis consequat",
+      price: 9,
+      quantity: 3,
+      id: "12345",
+    },
+    {
+      name: "Tvoluptatem",
+      price: 10.32,
+      quantity: 1,
+      id: "12345",
+    },
+    {
+      name: "Scelerisque lacinia",
+      price: 18.9,
+      quantity: 1,
+      id: "12345",
+    },
+    {
+      name: "Consectetur adipiscing",
+      price: 28.72,
+      quantity: 10,
+      id: "12345",
+    },
+    {
+      name: "Condimentum aliquet",
+      price: 13.9,
+      quantity: 1,
+      id: "12345",
+    },
+  ],
+  total: 357.32,
+};
+
+export const parsedLines = [
+  {
+    name: "Mollis consequat",
+    price: 9,
+    quantity: 3,
+    id: "12345",
+  },
+  {
+    name: "Tvoluptatem",
+    price: 10.32,
+    quantity: 1,
+    id: "12345",
+  },
+  {
+    name: "Scelerisque lacinia",
+    price: 18.9,
+    quantity: 1,
+    id: "12345",
+  },
+  {
+    name: "Consectetur adipiscing",
+    price: 28.72,
+    quantity: 10,
+    id: "12345",
+  },
+  {
+    name: "Condimentum aliquet",
+    price: 13.9,
+    quantity: 1,
+    id: "12345",
+  },
+];
